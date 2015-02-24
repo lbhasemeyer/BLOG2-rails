129f0c991ade72a9c0ba38edb5a0a84841db1c87
lkbhas <lbhasemeyer@gmail.com>
2015-02-23 21:22:47 -0700

    added users and connection to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213032440) do
+ActiveRecord::Schema.define(version: 20150224034655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20150213032440) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,24 @@
 Post.delete_all
+User.delete_all
+
+user1 = User.create!( first_name: Faker::Name.name, last_name: Faker::Name.name )
+user2 = User.create!( first_name: Faker::Name.name, last_name: Faker::Name.name )
+user3 = User.create!( first_name: Faker::Name.name, last_name: Faker::Name.name )
 
 post1 = Post.create!(
 title: Faker::Lorem.sentence,
 body: Faker::Lorem.paragraphs.join("\n\n"),
+user: user1
 )
 
 post2 = Post.create!(
 title: Faker::Lorem.sentence,
 body: Faker::Lorem.paragraphs.join("\n\n"),
+user: user1
 )
 
 post3 = Post.create!(
 title: Faker::Lorem.sentence,
 body: Faker::Lorem.paragraphs.join("\n\n"),
+user: user2
 )
